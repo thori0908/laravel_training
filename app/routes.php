8deacdba13beb051c9fb271d4d3cdf138e4c8694
thori0908 <mr.takumi.pc.0908@gmail.com>
2015-03-30 19:19:30 +0900

    add files#36

---
+++ app/routes.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('hello');
+});
+
+
+//Route::get('issue36_input', function()
+//{
+//    return View::make('home.issue36_input');
+//});
+//
+//Route::post('/issue36_confirm', function()
+//{
+//    return View::make('home.issue36_confirm');
+//});
+//
+//Route::post('/issue36_complete', function()
+//{
+//    return View::make('home.issue36_complete');
+//});
