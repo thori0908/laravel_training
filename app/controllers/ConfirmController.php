1f22d2cbc0eee9f919573fe9a23a6d8f1f311f0f
thori0908 <mr.takumi.pc.0908@gmail.com>
2015-04-14 16:00:19 +0900

    InputController:  入力画面に都道府県のセレクトボックスを追加  確認画面の入力値を入力画面へ受け渡して表示 ConfirmController:  入力画面の入力値を確認画面へ受け渡して表示。 User: フルネームを表示するメソッドを追加

--- app/controllers/ConfirmController.php
+++ app/controllers/ConfirmController.php
@@ -17,7 +17,37 @@ class ConfirmController extends BaseController {
 
     public function showConfirm()
     {  
-        return View::make('confirm_issue36');
+        $lastname  = Input::get('lastname', '');
+        $firstname = Input::get('firstname', '');
+        $gender    = Input::get('gender', '');
+        $postcodeFirst  = Input::get('postcodeFirst', '');
+        $postcodeSecond = Input::get('postcodeSecond', '');
+        $prefecture     = Input::get('prefecture', '');
+        $mailaddress    = Input::get('mailaddress', '');
+        $other      = Input::get('other', '');
+        $opinion    = Input::get('opinion', '');
+        $hobbyMusic = Input::get('hobbyMusic', '');
+        $hobbyMovie = Input::get('hobbyMovie', '');
+        $hobbyOther = Input::get('hobbyOther', '');
+        $hobbyOtherText = Input::get('hobbyOtherText', '');
+
+        $input_array = array();
+        $input_array['lastname']  = $lastname;
+        $input_array['firstname'] = $firstname;
+        $input_array['gender']    = $gender;
+        $input_array['postcodeFirst']  = $postcodeFirst;
+        $input_array['postcodeSecond'] = $postcodeSecond;
+        $input_array['prefecture']     = $prefecture;
+        $input_array['mailaddress']    = $mailaddress;
+        $input_array['other']      = $other;
+        $input_array['opinion']    = $opinion;
+        $input_array['hobbyMusic'] = $hobbyMusic;
+        $input_array['hobbyMovie'] = $hobbyMovie;
+        $input_array['hobbyOther'] = $hobbyOther;
+        $input_array['hobbyOtherText'] = $hobbyOtherText;
+
+        $user = new User($input_array);
+        return View::make('confirm')->with('user', $user);
     }
 
 }
--- app/models/User.php
+++ app/models/User.php
@@ -14,7 +14,14 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 *
 	 * @var string
 	 */
-	protected $table = 'users';
+	//protected $table = 'users';
+    protected $fillable = array('lastname',       'firstname',  'gender',      'postcodeFirst', 
+                                'postcodeSecond', 'prefecture', 'mailaddress', 'other', 
+                                'opinion',        'hobbyMusic', 'hobbyMovie',  'hobbyOther', 'hobbyOtherText');
+
+    public function getFullname() {
+        return sprintf('%s %s', $this->lastname, $this->firstname);
+    }
 
 	/**
 	 * The attributes excluded from the model's JSON form.
