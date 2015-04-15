4ba2ee16e505691a46d61ce65500d4325788805b
thori0908 <mr.takumi.pc.0908@gmail.com>
2015-04-15 13:29:37 +0900

    modify: Form::hogeを使った仕様に変更 ../confirm.blade.php ../input.blade.php

--- app/views/confirm.blade.php
+++ app/views/confirm.blade.php
@@ -6,53 +6,53 @@
     <table>
       <tr>
         <td>名前：{{ $user->getFullname() }}
-          <input type="hidden" name="lastname" value="{{ $user['lastname'] }}">
-          <input type="hidden" name="firstname" value="{{ $user['firstname'] }}">
+          {{ Form::hidden('lastname', $user['lastname'])}}
+          {{ Form::hidden('firstname', $user['firstname'])}}
         </td>
       </tr>
       <tr>
         <td>性別：{{ $user['gender'] }}
-          <input type="hidden" name="gender" value="{{ $user['gender'] }}">
+          {{ Form::hidden('gender', $user['gender'])}}
       </tr>
       <tr> 
         <td>郵便番号：{{ $user['postcodeFirst'] }} - {{ $user['postcodeSecond'] }}
-          <input type="hidden" name="postcodeFirst" value="{{ $user['postcodeFirst'] }}">
-          <input type="hidden" name="postcodeSecond" value="{{ $user['postcodeSecond'] }}">
+          {{ Form::hidden('gender', $user['gender'])}}
+          {{ Form::hidden('postcodeFirst', $user['postcodeFirst'])}}
         </td>
       </tr>
       <tr> 
         <td>都道府県：{{ $user['prefecture'] }}</td>
-          <input type="hidden" name="prefecture" value="{{ $user['prefecture'] }}">
+          {{ Form::hidden('prefecture', $user['prefecture'])}}
         </td>
       </tr>
       <tr> 
         <td>メールアドレス：{{ $user['mailaddress'] }}</td>
-          <input type="hidden" name="mailaddress" value="{{ $user['mailaddress'] }}">
+          {{ Form::hidden('mailaddress', $user['mailaddress'])}}
         </td>
       </tr>
       <tr> 
         <td>趣味：
           {{ $user['hobbyMusic'] }} {{ $user['hobbyMovie'] }} {{ $user['hobbyOther'] }} {{ $user['hobbyOtherText'] }}
-          <input type="hidden" name="hobbyMusic" value="{{ $user['hobbyMusic'] }}">
-          <input type="hidden" name="hobbyMovie" value="{{ $user['hobbyMovie'] }}">
-          <input type="hidden" name="hobbyOther" value="{{ $user['hobbyOther'] }}">
-          <input type="hidden" name="hobbyOtherText" value="{{ $user['hobbyOtherText'] }}">
+          {{ Form::hidden('hobbyMusic', $user['hobbyMusic'])}}
+          {{ Form::hidden('hobbyMovie', $user['hobbyMovie'])}}
+          {{ Form::hidden('hobbyOther', $user['hobbyOther'])}}
+          {{ Form::hidden('hobbyOtherText', $user['hobbyOtherText'])}}
         </td>
       </tr>
       <tr> 
         <td>ご意見：{{ nl2br($user['opinion']) }}</td>
-          <input type="hidden" name="opinion" value="{{ $user['opinion'] }}">
+          {{ Form::hidden('opinion', $user['opinion'])}}
         </td>
       </tr>
     </table>
-    <input name="return"  type="submit" value="戻る" formaction="input">
+    {{ Form::submit('戻る', ['formaction'=>"input"])}}
     <br>
   </form>
   <form action="/complete" method="POST">
-    <input type="hidden" name="lastname" value="{{ $user['lastname'] }}">
-    <input type="hidden" name="firstname" value="{{ $user['firstname'] }}">
-    <input type="hidden" name="mailaddress" value="{{ $user['mailaddress'] }}">
-    <input type="hidden" name="prefecture" value="{{ $user['prefecture'] }}">
-    <input type="submit" value="送信" formaction="/complete">
+    {{ Form::hidden('lastname', $user['lastname'])}}
+    {{ Form::hidden('firstname', $user['firstname'])}}
+    {{ Form::hidden('mailaddress', $user['mailaddress'])}}
+    {{ Form::hidden('prefecture', $user['prefecture'])}}
+    {{ Form::submit('送信', ['formaction'=>"/complete"])}}
   </form>
 @stop
--- app/views/input.blade.php
+++ app/views/input.blade.php
@@ -6,22 +6,32 @@
     <fieldset>
       <legend>フォーム</legend>
       <label for="name"> 名前：</label>
-      <input type="text" name="lastname" size="10" id="name" value="{{ $user->lastname }}" >
-      <input type="text" name="firstname" size="10" value="{{ $user->firstname }}">
+      {{ Form::text('lastname', $user->lastname, ['size'=>10]) }}
+      {{ Form::text('firstname', $user->firstname, ['size'=>10]) }}
       <font color="#ff0000">
       </font> 
       <font color="#ff0000">
       </font> 
       <br>
       <label for="male">性別：</label>
-      男性<input type="radio" name="gender" value="男" id="male" @if ( $user->gender == "男" ) {{ 'checked' }} @endif >
-      女性<input type="radio" name="gender" value="女" id="gender" @if ( $user->gender == "女" ) {{ 'checked' }}@endif >
+      男性
+      @if ( $user->gender == "男" ) 
+          {{ Form::radio('gender',  '男', true) }}
+      @else 
+          {{ Form::radio('gender',  '男') }}
+      @endif
+      女性
+      @if ( $user->gender == "女" ) 
+          {{ Form::radio('gender',  '女', true) }}
+      @else 
+          {{ Form::radio('gender',  '女') }}
+      @endif
       <font color="#ff0000">
       </font> 
       <br>  
       <label for="postcode">郵便番号：</label>
-      <input type="text" name="postcodeFirst" size="3" id="postcode" value="{{ $user->postcodeFirst }}">
-      - <input type="text" name="postcodeSecond" size="4" id="postcode" value="{{ $user->postcodeSecond }}">
+      {{ Form::text('postcodeFirst',  $user->postcodeFirst, ['size'=>4]) }}
+      - {{ Form::text('postcodeSecond',  $user->postcodeSecond, ['size'=>3]) }}
       <font color="#ff0000">
       </font> 
       <br>
@@ -34,21 +44,36 @@
       </font> 
       <br>        
       <label for="mailaddress">メールアドレス：</label>
-      <input type="text" name="mailaddress" size="40" id="mailaddress" value="{{ $user->mailaddress }}"><br>
+      {{ Form::text('mailaddress', $user->mailaddress, ['size'=>40]) }}
       <font color="#ff0000">
       </font> 
       <br>
       趣味：
-      <input type="checkbox" name="hobbyMusic" value="音楽鑑賞" id="music" @if ($user->hobbyMusic == "音楽鑑賞") {{ 'checked' }} @endif>音楽鑑賞
-      <input type="checkbox" name="hobbyMovie" value="映画鑑賞" id="movie" @if ($user->hobbyMovie == "映画鑑賞") {{ 'checked' }} @endif>映画鑑賞
-      <input type="checkbox" name="hobbyOther" value="その他" id="other"  @if ($user->hobbyOther == "その他") {{ 'checked' }} @endif>その他
-      <input type="text" name="hobbyOtherText" size="20" id="othertext" value="{{ $user->hobbyOtherText }}">
+      @if ( $user->hobbyMusic == "音楽鑑賞" ) 
+          {{ Form::checkbox('hobbyMusic',  '音楽鑑賞', true) }}
+      @else 
+          {{ Form::checkbox('hobbyMusic',  '音楽鑑賞') }}
+      @endif
+      音楽鑑賞
+      @if ( $user->hobbyMovie == "映画鑑賞" ) 
+          {{ Form::checkbox('hobbyMovie',  '映画鑑賞', true) }}
+      @else 
+          {{ Form::checkbox('hobbyMovie',  '映画鑑賞') }}
+      @endif
+      映画鑑賞
+      @if ( $user->hobbyOther == "その他" ) 
+          {{ Form::checkbox('hobbyOther',  'その他', true) }}
+      @else 
+          {{ Form::checkbox('hobbyOther',  'その他') }}
+      @endif
+      その他
+      {{ Form::text('hobbyOtherText', $user->hobbyOtherText, ['size'=>20]) }}
       <font color="#ff0000">
       </font> 
       <br>
       <label for="opinion">ご意見：</label>
       <textarea name="opinion" rows="2" cols="20" id="opinion">{{ $user->opinion }}</textarea><br>
-      <input type="submit" value="確認" id="opinion"><br>
+      {{ Form::submit('確認') }}
     </fieldset>
   </form> 
 @stop
