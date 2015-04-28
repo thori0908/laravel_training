5bbefae9da16435010bf5f659332a9caa653e568
thori0908 <mr.takumi.pc.0908@gmail.com>
2015-04-28 19:00:57 +0900

    ref #47　インデント訂正

--- app/tests/ErrMessageTest.php
+++ app/tests/ErrMessageTest.php
@@ -9,7 +9,7 @@ class ErrMessageTest extends TestCase
      */
 
 	public function testGetErrMessages($input, $expected)
-	{
+    {
         $errMessages = new ErrMessage();
         $validator = $errMessages->getErrMessages($input);
         $errMessage = $validator->messages()->first(key($input));
@@ -20,21 +20,21 @@ class ErrMessageTest extends TestCase
     {
         $name51 = str_repeat("あいうえお", 11);
         return array(
-                //     $input                        ,$expected 
-                 array(array('lastname' =>''),       '姓を入力して下さい'),
-                 array(array('lastname' =>'aaaa'),   '姓を全角で入力して下さい'),
-                 array(array('lastname' => $name51), '姓を50文字以内で入力して下さい'),
-                 array(array('firstname'=>''),       '名を入力して下さい'),
-                 array(array('firstname'=>'aasdf'),  '名を全角で入力して下さい'),
-                 array(array('firstname'=> $name51), '名を50文字以内で入力して下さい'),
-                 array(array('gender'=>'男'),        ''),
-                 array(array('gender'=>''),          '性別を選択して下さい'),
-                 array(array('postcodeFirst' =>''),  '郵便番号を入力して下さい'),
-                 array(array('postcodeSecond'=>''),  '郵便番号を入力して下さい'),
-                 array(array('pref_id'=>''),         '都道府県を選択してください'),
-                 array(array('pref_id'=>'default'),  '都道府県を選択してください'),
-                 array(array('mailaddress'=>''),     'メールアドレスを入力して下さい'),
-                 array(array('mailaddress'=>'aaa'),  'メールアドレスを正しく入力して下さい')
+               // $input                        ,$expected 
+            array(array('lastname' =>''),       '姓を入力して下さい'),
+            array(array('lastname' =>'aaaa'),   '姓を全角で入力して下さい'),
+            array(array('lastname' => $name51), '姓を50文字以内で入力して下さい'),
+            array(array('firstname'=>''),       '名を入力して下さい'),
+            array(array('firstname'=>'aasdf'),  '名を全角で入力して下さい'),
+            array(array('firstname'=> $name51), '名を50文字以内で入力して下さい'),
+            array(array('gender'=>'男'),        ''),
+            array(array('gender'=>''),          '性別を選択して下さい'),
+            array(array('postcodeFirst' =>''),  '郵便番号を入力して下さい'),
+            array(array('postcodeSecond'=>''),  '郵便番号を入力して下さい'),
+            array(array('pref_id'=>''),         '都道府県を選択してください'),
+            array(array('pref_id'=>'default'),  '都道府県を選択してください'),
+            array(array('mailaddress'=>''),     'メールアドレスを入力して下さい'),
+            array(array('mailaddress'=>'aaa'),  'メールアドレスを正しく入力して下さい')
         ); 
     }
 }
